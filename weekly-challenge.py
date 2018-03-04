de96496f1db1991c00bdee65b2c3fcabf1f9fb26
Daniel Jacobus Botha <20078196@sun.ac.za>
2018-03-04 22:22:29 +0200

    v1.0

--- weekly-challenge.py
+++ weekly-challenge.py
@@ -5,44 +5,58 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import sys
+import numpy as np
 
 # setup Bot
 client = Bot(description="Weekly Challenge Judge by iggnore", command_prefix=">", pm_help = False)
 
 @client.event
 async def on_ready():
-	
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
 
 	print('--------')
-
+	
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
 # Counts entries in #weekly-challenges
 @client.event
 async def on_message(message):
+	authors    = [] 
+	urls	   = []
+	votes	   = []
+
 	if not message.content.startswith('>count'):
 		return
 	
+	announcement_channel = client.get_channel('306891494726303746')
+
 	async for message in client.logs_from(message.channel):
 		for attachment in message.attachments:
 			if not attachment:
 				continue
 
-			print(message.author)
-			print(attachment['url'])
-			
+			authors.append(message.author)
+			urls.append(attachment['url'])		
+
 			for reaction in message.reactions:
 				if reaction.emoji == '❤':
-					print(reaction.count)
+					votes.append(reaction.count)
 
-			print('--')
+	indeces = np.argsort(votes)
 
-	embed = discord.Embed(title="Weekly Challenge Results", description="Congratulations to our #weekly-challenge winner!", color=discord.Colour.blue())
-	embed.add_field(name="Fist Place", value="Xenorra - {} votes".format(21), inline=True)
-	embed.add_field(name="Second Place", value="iggnore - {} votes".format(20), inline=True)
-	embed.add_field(name="Third Place", value="Malachaai - {} votes".format(10), inline=True)
+	one   = indeces[-1]
+	two   = indeces[-2]
+	three = indeces[-3]
 
-	await client.send_message(message.channel, embed=embed)
+	embed = discord.Embed(title="Weekly Challenge Results", description="@everyone congratulations to our <#{}> winner!".format(message.channel.id), color=discord.Colour.blue())
+	embed.add_field(name="First Place", value="<@!{}> - {}".format(authors[one].id, votes[one]), inline=True)
+	embed.add_field(name="Second Place", value="<@!{}> - {}".format(authors[two].id, votes[two]), inline=True)
+	embed.add_field(name="Third Place", value="<@!{}> - {}".format(authors[three].id, votes[three]), inline=True)
+	embed.set_footer(text='Thank you for all your entries! The next weekly challenge will be announced shortly.')
+
+	await client.send_message(message.channel, content='Results posted in <#{}>!'.format(announcement_channel.id))
+
+	await client.send_message(announcement_channel, embed=embed)
+
 client.run(str(sys.argv[1])) # Send API key via command line arguments
