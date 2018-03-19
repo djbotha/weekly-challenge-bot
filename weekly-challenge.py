34bd9d1f8c12ef1b327f6beae35078caf9e440ab
Daniel Jacobus Botha <20078196@sun.ac.za>
2018-03-19 16:49:06 +0200

    Added Google Drive functionality

--- weekly-challenge.py
+++ weekly-challenge.py
@@ -1,10 +1,13 @@
-# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
 import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import sys
+import requests
+import os
+import time
+import uploader
 from numpy import argsort
 
 # setup Bot
@@ -24,12 +27,22 @@ if not api_key:
 
 # close private key file
 key_file.close()
+
+def downloadimgs(authors, urls):
+	"""Download images from current challenge and save them as 
+		'author.jpg' in '/images/dd-mm-yyyy/'"""
+	dirname = time.strftime('%d-%m-%Y')
+	os.mkdir('images/'+dirname)
+	for i in range(len(authors)):
+		f = open('./images/'+dirname+'/'+authors[i].name+'.jpg', 'wb')  #create file locally
+		f.write(requests.get(urls[i]).content)  #write image content to this file 
+		f.close()
+
 @client.event
 async def on_ready():
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
 
 	print('--------')
-	
 	
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
@@ -37,9 +50,9 @@ async def on_ready():
 # Counts entries in #weekly-challenges
 @client.event
 async def on_message(message):
-	authors    = [] 
-	urls	   = []
-	votes	   = []
+	authors	= [] 
+	urls	= []
+	votes	= []
 
 	if not message.content.startswith('>count'):
 		return
@@ -66,21 +79,33 @@ async def on_message(message):
 
 	indices = argsort(votes)
 
-	one   = indices[-1]
-	two   = indices[-2]
-	three = indices[-3]
+	first	= indices[-1]
+	second  = indices[-2]
+	third	= indices[-3]
+	
+	message = await client.send_message(message.channel, content='Downloading images...')
+	downloadimgs(authors, urls) # download images from cdn.discordapp.com
+	await client.delete_message(message)
+	message = await client.send_message(message.channel, content='Downloads done.')
+	await client.delete_message(message)
+	message = await client.send_message(message.channel, content='Uploading images...')
+	link = uploader.upload()	# upload images to Google Drive
+	await client.delete_message(message)
+	message = await client.send_message(message.channel, content='Uploads done.')
+	await client.delete_message(message)
+	message = await client.send_message(message.channel, content='Images available at {} !'.format(link))
 
 	embed = discord.Embed(title="Weekly Challenge Results", description="Congratulations to our <#{}> winner!".format(message.channel.id), color=discord.Colour.blue())
-	embed.add_field(name="First Place", value="<@!{}> - {}".format(authors[one].id, votes[one]), inline=True)
-	embed.add_field(name="Second Place", value="<@!{}> - {}".format(authors[two].id, votes[two]), inline=True)
-	embed.add_field(name="Third Place", value="<@!{}> - {}".format(authors[three].id, votes[three]), inline=True)
+	embed.add_field(name="First Place", value="<@!{}> - {}".format(authors[first].id, votes[first]), inline=True)
+	embed.add_field(name="Second Place", value="<@!{}> - {}".format(authors[second].id, votes[second]), inline=True)
+	embed.add_field(name="Third Place", value="<@!{}> - {}".format(authors[third].id, votes[third]), inline=True)
 	embed.set_footer(text='Thank you for all your entries! The next weekly challenge will be announced shortly.')
 
 	# make a small announcement in the current channel
 	await client.send_message(message.channel, content='Results posted in <#{}>!'.format(announcement_channel.id))
 	
 	# send a PM to the winner
-	await client.send_message(authors[one], content='Hey {} - congratulations! You\'re the winner of this week\'s weekly challenge! A mod will DM you shortly with your prize.'.format(authors[one].name))
+	await client.send_message(authors[first], content='Hey {} - congratulations! You\'re the winner of this week\'s weekly challenge! A mod will DM you shortly with your prize.'.format(authors[first].name))
 
 	# make rich embedded announcement in the server's announcement channel
 	await client.send_message(announcement_channel, embed=embed)
