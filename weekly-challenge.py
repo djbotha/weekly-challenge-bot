ab2ccf1bb84968ef5dafcb0cdaf21ef7fe3b4df9
Daniel Jacobus Botha <20078196@sun.ac.za>
2018-07-02 17:05:34 +0200

    added playing status

--- weekly-challenge.py
+++ weekly-challenge.py
@@ -47,6 +47,8 @@ async def on_ready():
 	
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=9'.format(client.user.id))
+	return await client.change_presence(game=discord.Game(name='#weekly-challenge')) #This is buggy, let us know if it doesn't work.
+
 
 # Counts entries in #weekly-challenges
 @client.event
